7a37368de987a3b5d2ba3829e876b85ed7afb2dc
Mezan2002 <mezanurr009@gmail.com>
2022-12-11 23:06:19 +0600

    Project Card Done

--- src/Components/ProjectDetails/ProjectDetails.js
+++ src/Components/ProjectDetails/ProjectDetails.js
@@ -1,12 +1,81 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const ProjectDetails = () => {
   const location = useLocation();
-  const { name } = location?.state?.project;
+  const {
+    name,
+    image,
+    description,
+    gitHubClientLink,
+    gitHubServerLink,
+    usedTechnologies,
+    features,
+    projectImages,
+  } = location?.state?.project;
   return (
     <div className="card glass min-h-screen rounded-none text-white">
-      <h2>Hello From {name}</h2>
+      <div className="card my-10 w-8/12 mx-auto glass">
+        <div className="relative">
+          <figure>
+            <img src={image} alt="" />
+            <div className="absolute top-10 left-5">
+              <a title="back to home" className="btn rounded-full" href="/">
+                <i class="fa-solid fa-arrow-left"></i>
+              </a>
+            </div>
+          </figure>
+        </div>
+        <div className="card-body">
+          <h2 className="text-3xl font-bold text-black">
+            {name} by Mezanur Rahman
+          </h2>
+          <p className="text-black">{description}</p>
+          <div>
+            <h4 className="text-2xl text-black font-bold my-4">
+              Project Images
+            </h4>
+            <div className="">
+              {projectImages.map((projectImage, i) => (
+                <img
+                  key={i}
+                  src={projectImage}
+                  className="my-5 rounded-lg"
+                  alt=""
+                />
+              ))}
+            </div>
+          </div>
+          <div>
+            <h4 className="text-2xl font-bold my-4">User Technologies</h4>
+            <ul>
+              {usedTechnologies.map((technology, i) => (
+                <li className="list-disc list-inside" key={i}>
+                  {technology}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h4 className="text-2xl font-bold my-4">Features</h4>
+            <ul>
+              {features.map((feture, i) => (
+                <li className="list-disc list-inside" key={i}>
+                  {feture}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="card-actions">
+            <a className="btn btn-primary" href={gitHubClientLink}>
+              Client GitHub
+            </a>
+            <a className="btn btn-primary" href={gitHubServerLink}>
+              Server GitHub
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,37 +1,13 @@
-import React from "react";
-import project1 from "../../assets/images/skillUp.png";
-import project2 from "../../assets/images/khaiyalamu.png";
-import project3 from "../../assets/images/safeSale.png";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Projects = () => {
-  const projectsInfo = [
-    {
-      id: 1,
-      name: "Skill Up(A Skill Learning React App)",
-      description:
-        "I created that project with React JS, Firebase Authentication and some more interesting features.",
-      image: project1,
-      projectLink: "https://skill-up-by-mezan.web.app/",
-    },
-    {
-      id: 2,
-      name: "Khaiyalamu (A Cloud Kitchen React App)",
-      description:
-        "This project was created by React JS, Firebase,MongoDB and has some awesome features. It is cloud kitchen react app for selling foods",
-
-      image: project2,
-      projectLink: "https://khaiyalamu-by-mezan.web.app/",
-    },
-    {
-      id: 3,
-      name: "Safe Sale (Used Mobile Resale website)",
-      description:
-        "This project was designed by Tailwind CSS and daisy UI, This is a MERN stack website. There are some interesting functionalities on it. Go and explore it.",
-      image: project3,
-      projectLink: "https://safesalebymezan.web.app/",
-    },
-  ];
+  const [projectsInfo, setProjectsInfo] = useState([]);
+  useEffect(() => {
+    fetch("ProjectDetails.json")
+      .then((res) => res.json())
+      .then((data) => setProjectsInfo(data));
+  }, []);
   return (
     <div className="md:my-20 my-10" id="projects">
       <h2 className="md:text-4xl text-xl text-center font-bold mb-5 md:mb-10 text-white">
