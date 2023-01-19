0ce24ae55e975af3cdf2ec6e93e548502f3c3d7c
Mezan2002 <mezanurr009@gmail.com>
2023-01-19 23:07:38 +0600

    Skills is added

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Skills = () => {
+  const [skills, setSkills] = useState([]);
+  useEffect(() => {
+    fetch("skills.json")
+      .then((res) => res.json())
+      .then((data) => setSkills(data));
+  }, []);
   return (
     <div id="skills">
       <div className="md:my-20 my-10">
@@ -8,16 +14,17 @@ const Skills = () => {
           Skills
         </h2>
         <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-10 md:px-20">
-          <div className="card bg-white shadow-xl">
-            <div className="card-body">
-              <img
-                src="https://i.ibb.co/fHwTJfW/html-5-1.png"
-                alt=""
-                className="w-20 mx-auto"
-              />
-              <h2 className="text-2xl font-bold text-center">HTML</h2>
+          {skills.map((skill) => (
+            <div
+              key={skill.id}
+              className="card bg-white hover:bg-base-300 hover:scale-90 duration-300 shadow-xl"
+            >
+              <div className="card-body">
+                <img src={skill.icon} alt="" className="w-20 h-20 mx-auto" />
+                <h2 className="text-2xl font-bold text-center">{skill.name}</h2>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
